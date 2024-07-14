37653a4eaabe44846d5ca17bfb771b3a59ceb75b
Dmitry Nedelkin <dmitry.nedelkin@NetCracker.com>
2024-07-14 22:33:14 +0300

    version 2: grpc server added

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"testproject/grpc_server"
+	"testproject/rest_server"
+)
+
+func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go rest_server.StartRestServer()
+	go grpc_server.StartGrpsServer()
+
+	<-stop
+	log.Println("Shutting down servers...")
+}
